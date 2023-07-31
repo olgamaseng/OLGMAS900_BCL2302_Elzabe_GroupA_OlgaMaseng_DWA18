0d9930658a2a3f6ff18c77769e74f9e19e75a958
Olga Maseng <masengolga@gmail.com>
2023-07-31 20:38:51 +0200

    Creating the nav bar

--- PODCAST/src/App.jsx
+++ PODCAST/src/App.jsx
@@ -1,15 +1,19 @@
+import React from 'react';
+import FetchAPI from './Components/FetchApi.jsx';
+import PrimarySearchAppBar from './Components/PrimarySearchAppBar.jsx'
 
 
-function App() {
-
-
+function App()  {
   return (
-    <>
-     
-      <h1>MM</h1>
-      
-    </>
-  )
+    <div>
+       <PrimarySearchAppBar />
+       <FetchAPI />
+       {/* <SignInSide /> */}
+      {/* <NavBar /> */}
+      {/* Other components and content */}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
---
+++ PODCAST/src/Components/FetchApi.jsx
@@ -0,0 +1,33 @@
+import React from 'react' 
+import {useState, useEffect} from 'react'
+import MyComponent from './Grid'
+
+export default function FetchAPI() { 
+    
+    const url = "https://podcast-api.netlify.app/shows" 
+
+    const [dataId, setDataId] = useState([]) 
+
+    useEffect(() => {
+        fetch(url) 
+        .then((response) => response.json())
+         .then((data) => {
+         const fetching = data.map((show) => { 
+            return( 
+            <div key={show.id}> 
+         <p>{show.id}</p> 
+         {/* <p>{show.description}</p> 
+         <h1>{show.title}</h1>
+          <p>{show.genres}</p>
+           <p>{show.updated}</p>  */}
+           <img src={show.image} width = "50%"/> 
+           {/* <h1>{show.seasons}</h1>  */}
+           <MyComponent />
+           </div> )
+            }); 
+           
+           setDataId(fetching); }) }, 
+           []);
+            return <div>{dataId}</div> }
+
+            
